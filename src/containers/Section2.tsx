c80efc83d169e1c5a176f0775d5d7d320a65dbbd
Joshua Opata <opatajoshua@gmail.com>
2021-12-02 14:16:13 +0000

    styled section 2

--- src/containers/Section2.tsx
+++ src/containers/Section2.tsx
@@ -2,10 +2,13 @@
 function Section2({ names }: { names: string[] }) {
 
   return (
-    <div className="container max-w-4xl mx-auto lg:px-0 px-6 pb-28">
-      <ul>
+    <div className="">
+      <ul className="name-list mt-8 divide-y">
         {names.map((n, index) => (
-        <li key={index}>{n}</li>
+        <li key={index} className="flex items-center py-3">
+          <div className="font-bold text-xl rounded-full bg-blue-50 text-blue-800 w-8 h-8 flex items-center justify-center mr-3">{index+1}</div> 
+          <span className="font-medium text-md text-gray-700">{n}</span>
+        </li>
         ))}
       </ul>
     </div>
