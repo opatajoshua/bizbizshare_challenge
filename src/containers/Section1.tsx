1537afcad8945fa1797efbf595f98ffe79c52800
Joshua Opata <opatajoshua@gmail.com>
2021-12-02 14:16:04 +0000

    styles section 1

--- src/containers/Section1.tsx
+++ src/containers/Section1.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
-interface Section1Props{
-  onNewName: (name:string) => void,
-  onNewAnimal: (animal:string) => void
+interface Section1Props {
+  onNewName: (name: string) => void,
+  onNewAnimal: (animal: string) => void
 }
 
 /** Section1 container */
-function Section1({onNewName, onNewAnimal}: Section1Props) {
+function Section1({ onNewName, onNewAnimal }: Section1Props) {
   const [name, setName] = useState<string>('');
   const [favAnimal, setFavAnimal] = useState<string>('');
 
   function submitName(e: React.FormEvent<EventTarget>) {
     e.preventDefault();
     onNewName(name);
-    setName(''); 
+    setName('');
   }
 
   function submitFavAnimal(e: React.FormEvent<EventTarget>) {
@@ -23,17 +23,25 @@ function Section1({onNewName, onNewAnimal}: Section1Props) {
   }
 
   return (
-    <div className="container max-w-4xl mx-auto lg:px-0 px-6 pb-28">
+    <div className="">
       {/* form 1 */}
-      <form onSubmit={submitName}>
-        <input required type="text" value={name} onInput={(e)=>setName((e.target as any).value)}/>
-        <button type="submit">Submit</button>
+      <form onSubmit={submitName} className="flex flex-col  py-10">
+        <label htmlFor="nameInput" className="w-full">Name</label>
+        <div className="flex flex-wrap">
+          <input id="nameInput" placeholder="Enter name" required type="text" value={name} onInput={(e) => setName((e.target as any).value)}
+              className="bg-gray-100 rounded-md h-12 px-3 flex-1 max-w-md  mr-0 md:mr-4" />
+          <button type="submit" className="h-12 bg-blue-600 text-white font-bold rounded-md px-10 hover:bg-opacity-90 w-full md:w-auto mt-4 md:mt-0">Submit</button>
+        </div>
       </form>
 
       {/* form 2 */}
-      <form onSubmit={submitFavAnimal}>
-        <input required type="text" value={favAnimal}  onInput={(e)=>setFavAnimal((e.target as any).value)}/>
-        <button type="submit">Submit</button>
+      <form onSubmit={submitFavAnimal} className="flex flex-col  py-10">
+        <label htmlFor="favAnimalInput" className="w-full">Favorite animal</label>
+        <div className="flex flex-wrap">
+          <input id="favAnimalInput" placeholder="Enter your favorite animal" required type="text" value={favAnimal} onInput={(e) => setFavAnimal((e.target as any).value)}
+          className="bg-gray-100 rounded-md h-12 px-3 flex-1 max-w-md  mr-0 md:mr-4" />
+          <button type="submit" className="h-12 bg-blue-600 text-white font-bold rounded-md px-10 hover:bg-opacity-90 w-full md:w-auto mt-4 md:mt-0">Submit</button>
+        </div>
       </form>
     </div>
   );
